f1eb38b15fac14978aa3912e255c004f77d2c7a0
Benjamin Yvernault <byvernault@gmail.com>
2016-02-24 15:35:52 +0000

    Adding ucl_importer.py and editing ucl_processing_conf.sh

---
+++ ucl_importer.py
@@ -0,0 +1,19 @@
+'''
+    Author:         Benjamin Yvernault
+    contact:        b.yvernault@ucl.ac.uk
+    Creation date:  2016-02-24 15:31:05.774006
+    Purpose:        Importer for UCL processors and modules
+'''
+
+__author__ = "Benjamin Yvernault"
+__email__ = "b.yvernault@ucl.ac.uk"
+__purpose__ = "Importer for UCL processors/modules for dax"
+__modifications__ = "2016-02-24 15:31:05.774006 - Original write"
+
+# Import all the processors
+from processors.Processor_GIF_Parcellation import Processor_GIF_Parcellation
+from processors.Processor_Registration2Ref import Processor_Registration2Ref
+
+#Import all the modules
+# None so far
+
