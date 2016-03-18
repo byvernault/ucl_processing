647f37f7a01fc9b85dd02fe40524778e4c1a8a7e
Benjamin Yvernault <byvernault@gmail.com>
2016-03-18 10:52:14 +0000

    Adding Processor_Reg_ADC_2_T2

--- ucl_processors/__init__.py
+++ ucl_processors/__init__.py
@@ -1,2 +1,3 @@
 from Processor_GIF_Parcellation import Processor_GIF_Parcellation
 from Processor_Registration2Ref import Processor_Registration2Ref
+from Processor_Reg_ADC_2_T2 import Processor_Reg_ADC_2_T2
