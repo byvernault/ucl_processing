514d9759d35c27340e3b322e27f8fc541d3b64e9
byvernault <byvernault@gmail.com>
2016-10-18 16:39:38 +0100

    New spider version for VERDICT. DICOM produced via MATLAB with color for 4 of them. Removing unused variable in v1.0.0

--- ucl_spiders/Spider_Verdict_v1_0_1.py
+++ ucl_spiders/Spider_Verdict_v1_0_1.py
@@ -12,13 +12,9 @@ Purpose:        Generate Verdict Map from all Verdict scans registered \
 # Python packages import
 import os
 import sys
-import dicom
 import shutil
-import datetime
 import numpy as np
 import nibabel as nib
-from dicom.sequence import Sequence
-from dicom.dataset import Dataset, FileDataset
 from dax import XnatUtils, spiders, SessionSpider
 
 __author__ = "Benjamin Yvernault"
@@ -43,6 +39,15 @@ launch_AMICO_for_INNOVATE('{input_path}',\
 '{spams}',\
 '{scheme_filename}');
 """
+NII2DICOM = """
+nifti_folder = '{maps_folder}';
+dicom_file = '{dicom_file}';
+matlab_code = '{matlab_code}';
+out_folder = '{out_folder}';
+nb_acq = {acq};
+
+nii2dicomRGB(nifti_folder, dicom_file, out_folder, matlab_code, nb_acq)
+"""
 DEFAULT_PDF_MAKER = """
 % Code to generate PDF for VERDICT MAP
 
@@ -64,47 +69,6 @@ end
 """
 DICOM_SCAN_TYPE = ['WIP b3000_90 SENSE', 'SWITCH DB TO YES b3000_80',
                    'b3000_80']
-ORDER_MAPS = ['FIT_fIC', 'FIT_cellularity',
-              'FIT_fEES', 'FIT_fVASC', 'FIT_R',
-              'FIT_FobjCamino', 'FIT_Fobj', 'FIT_dir',
-              'FIT_R_0-3u', 'FIT_R_3-6u', 'FIT_R_6-9u',
-              'FIT_R_9-12u', 'FIT_R_12-15u',
-              'FIT_fIC_0-3u', 'FIT_fIC_3-6u', 'FIT_fIC_6-9u',
-              'FIT_fIC_9-12u', 'FIT_fIC_12-15u',
-              'FIT_cell_0-3u', 'FIT_cell_3-6u', 'FIT_cell_6-9u',
-              'FIT_cell_9-12u', 'FIT_cell_12-15u']
-C_RANGE = {'FIT_fIC': {'min': 0, 'max': 1, 'index': 1},
-           'FIT_cellularity': {'min': 2*10**11, 'max': 1.5*10**14,
-                               'index': 2},
-           'FIT_fEES': {'min': 0, 'max': 1, 'index': 3},
-           'FIT_fVASC': {'min': 0, 'max': 1, 'index': 4},
-           'FIT_R': {'min': 0, 'max': 15.10*10**-6, 'index': 5},
-           'FIT_FobjCamino': {'min': 0, 'max': 50, 'index': 10},
-           'FIT_Fobj': {'min': 0, 'max': 1, 'index': 11},
-           'FIT_dir': {'min': 1*10**-10, 'max': 2.9*10**-9, 'index': 12},
-           'FIT_R_0-3u': {'min': 0, 'max': 2.67*10**-6, 'index': 13},
-           'FIT_R_3-6u': {'min': 3.56*10**-6, 'max': 5.34*10**-6,
-                          'index': 14},
-           'FIT_R_6-9u': {'min': 6.22*10**-6, 'max': 8.89*10**-6,
-                          'index': 15},
-           'FIT_R_9-12u': {'min': 9.77*10**-6, 'max': 11.55*10**-6,
-                           'index': 16},
-           'FIT_R_12-15u': {'min': 12.44*10**-6, 'max': 15.10*10**-6,
-                            'index': 17},
-           'FIT_fIC_0-3u': {'min': 0, 'max': 1, 'index': 18},
-           'FIT_fIC_3-6u': {'min': 0, 'max': 1, 'index': 19},
-           'FIT_fIC_6-9u': {'min': 0, 'max': 1, 'index': 20},
-           'FIT_fIC_9-12u': {'min': 0, 'max': 1, 'index': 21},
-           'FIT_fIC_12-15u': {'min': 0, 'max': 1, 'index': 22},
-           'FIT_cell_0-3u': {'min': 3*10**12, 'max': 5*10**16, 'index': 23},
-           'FIT_cell_3-6u': {'min': 2*10**12, 'max': 3*10**14, 'index': 24},
-           'FIT_cell_6-9u': {'min': 1*10**12, 'max': 2.5*10**14,
-                             'index': 25},
-           'FIT_cell_9-12u': {'min': 1*10**12, 'max': 5*10**13,
-                              'index': 26},
-           'FIT_cell_12-15u': {'min': 2*10**11, 'max': 5.5*10**13,
-                               'index': 27}
-           }
 
 
 def parse_args():
@@ -255,17 +219,18 @@ class Spider_Verdict(SessionSpider):
             if not os.path.isfile(self.inputs['dcm']):
                 err = "DICOM File %s not found."
                 raise Exception(err % self.inputs['dcm'])
-            sour_obj = dicom.read_file(self.inputs['dcm'])
 
             # Convert all niftis to dicoms
-            convert_niftis_2_dicoms(
-                outdir,
-                sour_obj,
-                osirix_folder,
-                nb_acq)
-
-            # Subtract the Cobj to the maps:
-            subtract_obj_to_map(outdir, sour_obj, osirix_folder, nb_acq)
+            mat_lines = NII2DICOM.format(
+                    matlab_code=self.matlab_code,
+                    maps_folder=os.path.join(output_folder, str(nb_acq),
+                                             'AMICO', 'VerdictProstate_Rmaps'),
+                    dicom_file=self.inputs['dcm'],
+                    output_folder=osirix_folder,
+                    acq=str(nb_acq))
+            nii2dcm_script = os.path.join(output_folder,
+                                          'run_nii2dcm_%d.m' % nb_acq)
+            XnatUtils.run_matlab(nii2dcm_script, verbose=True)
 
         # Make pdf:
         self.make_pdf()
@@ -347,196 +312,6 @@ class Spider_Verdict(SessionSpider):
             cmd = cmd+' > '+os.path.join(matlabdir, prefix+'_outlog.log')
         os.system(cmd)
         self.time_writer("Matlab script: %s done" % matlab_script)
-
-
-def write_dicom(pixel_array, filename, ds_ori,
-                series_number, sop_id, series_description):
-    """Write a dicom from a pixel_array (numpy).
-
-    :param pixel_array: 2D numpy ndarray.
-                        If pixel_array is larger than 2D, errors.
-    :param filename: string name for the output file.
-    :param ds_ori: original pydicom object of the pixel_array
-    :param series_number: number of the series being processed
-    :param sop_id: SOPInstanceUID for the DICOM
-    :param series_description: series description for Osirix display
-    """
-    # Set the DICOM dataset
-    file_meta = Dataset()
-    file_meta.MediaStorageSOPClassUID = 'Secondary Capture Image Storage'
-    file_meta.MediaStorageSOPInstanceUID = ds_ori.SOPInstanceUID
-    file_meta.ImplementationClassUID = ds_ori.SOPClassUID
-    ds = FileDataset(filename, {}, file_meta=file_meta, preamble="\0"*128)
-
-    # Copy the tag from the original DICOM
-    for tag, d_obj in ds_ori.items():
-        if tag != ds_ori.data_element("PixelData").tag:
-            ds[tag] = d_obj
-
-    # Other tags to set
-    ds.SeriesNumber = series_number
-    sop_uid = sop_id + str(datetime.datetime.now()).replace('-', '')\
-                                                   .replace(':', '')\
-                                                   .replace('.', '')\
-                                                   .replace(' ', '')
-    ds.SOPInstanceUID = sop_uid[:-1]
-    ds.ProtocolName = '%s Verdict MAP' % series_description
-    # Set SeriesDate/ContentDate
-    now = datetime.date.today()
-    ds.SeriesDate = '%d%02d%02d' % (now.year, now.month, now.day)
-    ds.ContentDate = '%d%02d%02d' % (now.year, now.month, now.day)
-    ds.Modality = 'MR'
-    ds.ConversionType = 'WSD'
-    ds.StudyDescription = 'INNOVATE'
-    ds.SeriesDescription = series_description
-    ds.AcquisitionNumber = 1
-    ds.SamplesperPixel = 1
-    ds.PhotometricInterpretation = 'RGB'
-    # ds.PhotometricInterpretation = 'MONOCHROME2'
-    ds.SecondaryCaptureDeviceManufctur = 'Python 2.7.3'
-    nb_frames = pixel_array.shape[2]*pixel_array.shape[3]
-    ds.NumberOfFrames = nb_frames
-    ds.PixelRepresentation = 0
-    ds.HighBit = 15
-    ds.BitsStored = 8
-    ds.BitsAllocated = 8
-    ds.SmallestImagePixelValue = pixel_array.min()
-    ds.LargestImagePixelValue = pixel_array.max()
-    ds.Columns = pixel_array.shape[0]
-    ds.Rows = pixel_array.shape[1]
-
-    # Fixing the sequence if the number of frames was less than the original
-    # it happens if we remove the last volume for phillips data (mean)
-    if ds_ori.NumberOfFrames > nb_frames:
-        new_seq = Sequence()
-        for i in xrange(0, ds_ori.NumberOfFrames):
-            if i % 5 == 0:  # take one slice for each (14)
-                new_seq.append(ds_ori[0x5200, 0x9230][i])
-        ds[0x5200, 0x9230].value = new_seq
-
-    # Organise the array:
-    pixel_array2 = np.zeros((pixel_array.shape[0]*pixel_array.shape[2],
-                             pixel_array.shape[1]))
-    for i in range(pixel_array.shape[2]):
-        pixel_array2[pixel_array.shape[0]*i:pixel_array.shape[0]*(i+1),
-                     :] = pixel_array[:, :, i, 0]
-
-    # Set the Image pixel array
-    if pixel_array2.dtype != np.uint8:
-        pixel_array2 = pixel_array2.astype(np.uint8)
-
-    ds.PixelData = pixel_array2.tostring()
-    # Save the image
-    ds.save_as(filename)
-
-
-def convert_niftis_2_dicoms(nifti_folder, sour_obj, output_folder, nbacq):
-    """Convert 3D niftis into DICOM files.
-
-    :param nifti_path: path to the nifti file
-    :param sour_obj: pydicom object for the source dicom
-    :param output_folder: folder where the DICOM files will be saved
-    :param nbacq: Acquisition number
-    :return: None
-    """
-    if not os.path.isdir(nifti_folder):
-        raise Exception("NIFTY Folder %s not found." % nifti_folder)
-    for maps_name in ORDER_MAPS:
-        # File
-        nii_map = os.path.join(nifti_folder, '%s.nii' % maps_name)
-        if not os.path.isfile(nii_map):
-            raise Exception("NIFTY file %s not found." % nii_map)
-        # Naming
-        label = os.path.basename(nii_map)[:-4]
-        series_description = '%s_%d_original' % (label, nbacq)
-
-        # Edit Niftys
-        f_img = nib.load(nii_map)
-        f_data = f_img.get_data()
-        # Rotation 270
-        f_data = np.rot90(f_data)
-        f_data = np.rot90(f_data)
-        f_data = np.rot90(f_data)
-        # Normalizing data:
-        dmin = C_RANGE[label]['min']
-        dmax = C_RANGE[label]['max']
-        f_data[f_data < dmin] = dmin
-        f_data[f_data > dmax] = dmax
-        # Scale numbers to uint8
-        f_data = (f_data - dmin)*255.0/(dmax - dmin)
-
-        # Make output_folder:
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        # Series Number and SOP UID
-        series_number = 87000 + C_RANGE[label]['index']
-        sop_id = sour_obj.SOPInstanceUID.split('.')
-        sop_id = '.'.join(sop_id[:-1])+'.'
-
-        # Write the dicom
-        filename = os.path.join(output_folder, '%s.dcm' % series_description)
-        write_dicom(f_data, filename, sour_obj,
-                    series_number, sop_id, series_description)
-
-
-def subtract_obj_to_map(nii_folder, sour_obj, output_folder, nbacq):
-    if not os.path.isdir(nii_folder):
-        raise Exception("NIFTY Folder %s not found." % nii_folder)
-
-    nii_mapobj = os.path.join(nii_folder, 'FIT_FobjCamino.nii')
-    if not os.path.isfile(nii_mapobj):
-        raise Exception("NIFTY file OBJ %s not found." % nii_mapobj)
-
-    # Load image from NIFTI
-    f_img_mobj = nib.load(nii_mapobj)
-    f_data_obj = f_img_mobj.get_data()
-    # Rotation 270
-    f_data_obj = np.rot90(f_data_obj)
-    f_data_obj = np.rot90(f_data_obj)
-    f_data_obj = np.rot90(f_data_obj)
-    mask = f_data_obj > C_RANGE['FIT_FobjCamino']['max']
-
-    for index, map_name in enumerate(['FIT_fIC', 'FIT_cellularity',
-                                      'FIT_fEES', 'FIT_fVASC', 'FIT_R']):
-        # File
-        nii_map = os.path.join(nii_folder, '%s.nii' % map_name)
-        if not os.path.isfile(nii_map):
-            raise Exception("NIFTY file %s not found." % nii_map)
-        # Load image from NIFTI
-        f_img = nib.load(nii_map)
-        f_data = f_img.get_data()
-        # Rotation 270
-        f_data = np.rot90(f_data)
-        f_data = np.rot90(f_data)
-        f_data = np.rot90(f_data)
-        # Normalizing data:
-        dmin = C_RANGE[map_name]['min']
-        dmax = C_RANGE[map_name]['max']
-        f_data[f_data < dmin] = dmin
-        f_data[f_data > dmax] = dmax
-
-        # Scale numbers to uint8
-        f_data = (f_data - dmin)*255.0/(dmax - dmin)
-
-        # Subtract by setting all value to nan:
-        f_data[mask] = np.nan
-
-        # Make output_folder:
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        # map_name:
-        series_description = '%s_%d_subtracted' % (map_name, nbacq)
-
-        # Series Number and SOP UID
-        series_number = 87000 + index + 5
-        sop_id = sour_obj.SOPInstanceUID.split('.')
-        sop_id = '.'.join(sop_id[:-1])+'.'
-
-        filename = os.path.join(output_folder, '%s.dcm' % series_description)
-        write_dicom(f_data, filename, sour_obj,
-                    series_number, sop_id, series_description)
 
 
 if __name__ == '__main__':
