9434f61b79f728002e2c2a7295130b5eace660d4
byvernault <byvernault@gmail.com>
2016-12-06 15:27:15 +0000

    editing new spider and creating processor.

---
+++ ucl_processors/Processor_ADC_MAP.py
@@ -0,0 +1,117 @@
+"""Processor associated to Spider_ADC_MAP.
+
+Author:         Benjamin Yvernault
+contact:        b.yvernault@ucl.ac.uk
+Processor name: Processor_ADC_MAP
+Creation date:  2016-08-18 13:14:30.689905
+Purpose:        Generate ADC map from Verdict registered scans
+"""
+
+# Python packages import
+import os
+import logging
+from dax import XnatUtils, SessionProcessor
+
+__author__ = "Benjamin Yvernault"
+__email__ = "b.yvernault@ucl.ac.uk"
+__purpose__ = "Generate ADC map from Verdict registered scans"
+__processor_name__ = "Processor_Verdict"
+__modifications__ = """2016-08-18 13:14:30.689905 - Original write"""
+
+# set-up logger for printing statements
+LOGGER = logging.getLogger('dax')
+
+# Default values for arguments:
+# EDIT PARAMETERS FOR YOUR SPIDER CASE (SPIDER_PATH, WALLTIME, etc...)
+HOME = os.path.expanduser("~")
+DEFAULT_SPIDER_PATH = os.path.join(HOME, 'Xnat-management/ucl_processing/\
+ucl_spiders/', 'Spider_ADC_MAP_v1_0_0.py')
+DEFAULT_WALLTIME = '00:30:00'
+DEFAULT_MEM = 6048
+DEFAULT_MATLAB_CODE = os.path.join(HOME, 'Code', 'matlab')
+DEFAULT_SCAN_TYPES = [
+    'WIP b3000_90 SENSE', 'SWITCH DB TO YES b3000_80', 'b3000_80',
+    'b2000_vx1.3', 'b1500_vx1.3', 'b500_vx1.3', 'b90_vx1.3']
+
+# Format for the spider command line
+SPIDER_FORMAT = """python {spider} \
+-p {proj} \
+-s {subj} \
+-e {sess} \
+-c {scan} \
+-d {dir} \
+--suffix "{suffix_proc}" \
+--mc {mc} \
+"""
+
+
+class Processor_ADC_MAP(SessionProcessor):
+    """Processor class for Verdict that runs on a session.
+
+    :param spider_path: spider path on the system
+    :param version: version of the spider
+    :param walltime: walltime required by the spider
+    :param mem_mb: memory in Mb required by the spider
+
+    :param suffix: suffix to the spider
+    """
+
+    def __init__(self, spider_path=DEFAULT_SPIDER_PATH, version=None,
+                 scan_types=DEFAULT_SCAN_TYPES,
+                 matlab_code=DEFAULT_MATLAB_CODE,
+                 walltime=DEFAULT_WALLTIME, mem_mb=DEFAULT_MEM,
+                 suffix_proc=''):
+        """Entry point for Processor_Verdict Class."""
+        super(Processor_ADC_MAP,
+              self).__init__(scan_types, walltime, mem_mb, spider_path,
+                             version, suffix_proc=suffix_proc)
+        self.mc = matlab_code
+
+    def has_inputs(self, cscan):
+        """Method overridden from base class.
+
+        By definition:
+            status = 0  -> NEED_INPUTS,
+            status = 1  -> NEED_TO_RUN
+            status = -1 -> NO_DATA
+            qcstatus needs a value only when -1 or 0.
+        You need to set qcstatus to a short string that explain
+        why it's no ready to run. e.g: No NIFTI
+
+        :param cscan: object cscan define in dax.XnatUtils
+                      (see XnatUtils in dax for information)
+        :return: status, qcstatus
+        """
+        if XnatUtils.is_cscan_unusable(cscan):
+            return -1, 'Scan unusable'
+
+        # Check has_resource PARREC or NIFTI
+        if XnatUtils.has_resource(cscan, 'DICOM'):
+            return 1, None
+
+        LOGGER.debug('ADC MAP: DICOM not found.')
+        return 0, 'No DICOM'
+
+    def get_cmds(self, assessor, jobdir):
+        """Method to generate the spider command for cluster job.
+
+        :param assessor: pyxnat assessor object
+        :param jobdir: jobdir where the job's output will be generated
+        :return: command to execute the spider in the job script
+        """
+        proj_label = assessor.parent().parent().parent().label()
+        subj_label = assessor.parent().parent().label()
+        sess_label = assessor.parent().label()
+        assr_label = assessor.label()
+        scan_label = assr_label.split('-x-')[3]
+
+        cmd = SPIDER_FORMAT.format(spider=self.spider_path,
+                                   proj=proj_label,
+                                   subj=subj_label,
+                                   sess=sess_label,
+                                   scan=scan_label,
+                                   dir=jobdir,
+                                   mc=self.mc,
+                                   suffix_proc=self.suffix_proc)
+
+        return [cmd]
--- ucl_spiders/Spider_ADC_MAP_v1_0_0.py
+++ ucl_spiders/Spider_ADC_MAP_v1_0_0.py
@@ -11,7 +11,9 @@ Purpose:        Compute ADC Maps for HandN project
 # Python packages import
 import os
 import sys
-from dax import XnatUtils, spiders, ScanSpider, SessionSpider
+import glob
+import subprocess as sb
+from dax import XnatUtils, spiders, ScanSpider
 
 __author__ = "Benjamin Yvernault & Vasia Papoutsaki"
 __email__ = "b.yvernault@ucl.ac.uk"
@@ -19,6 +21,17 @@ __purpose__ = "Compute ADC Maps for HandN project"
 __spider_name__ = "ADC_MAP"
 __version__ = "1.0.0"
 __modifications__ = """2016-12-01 13:49:23.748867 - Original write"""
+
+DEFAULT_ADC_TEMPLATE = """
+addpath(genpath('{matlab_code}'));
+XNAT_ADCsMaps('{input_path}',\
+'{output}',\
+'{pdf_name}');
+"""
+
+FSLSWAP_VAL = {0: 'x',
+               1: 'y',
+               2: 'z'}
 
 
 def parse_args():
@@ -82,48 +95,119 @@ class Spider_ADC_MAP(ScanSpider):
 
         :param argument_parse: argument parser object return by parse_args()
         """
-        resource = 'XXXX'  # resource to download from the scan on XNAT
-        folder = os.path.join(self.jobdir, 'inputs')
-        os.makedirs(folder)
-        self.inputs.extend(self.download(self.xnat_scan, resource, folder))
-
-        #
-        # YOU CAN ADD MORE line to download other data LIKE
-        #    self.inputs.extend(self.download(**kwargs))
-        #
+        resource = 'DICOM'  # resource to download from the scan on XNAT
+        input_dir = XnatUtils.makedir(os.path.join(self.jobdir, 'inputs'))
+        self.inputs.extend(self.download(self.xnat_scan, resource, input_dir))
+        if len(self.inputs) == 1 and self.inputs[0].endswith('.zip'):
+            os.system('unzip -d %s -j %s > /dev/null'
+                      % (input_dir, self.inputs[0]))
+            os.remove(self.inputs[0])
+        self.inputs = get_dicom_list(input_dir)
 
     def run(self):
         """Method running the process for the spider on the inputs data."""
-        #
-        # CODE THAT YOU WANT TO RUN ON THE DATA
-        #
+        output_folder = XnatUtils.makedir(os.path.join(self.jobdir, 'outputs'))
+        mat_lines = DEFAULT_ADC_TEMPLATE.format(
+                matlab_code=self.matlab_code,
+                input_path=os.path.dirname(self.inputs[0]),
+                output=output_folder,
+                pdf_name=self.pdf_final)
+        matlab_script = os.path.join(output_folder, 'run_matlab_verdict.m')
+        with open(matlab_script, "w") as f:
+            f.writelines(mat_lines)
+        self.run_matlab(matlab_script, verbose=True)
+
+        # Zip outputs:
+        for folder in os.listdir(output_folder):
+            fpath = os.path.join(output_folder, folder)
+            if os.path.isdir(fpath):
+                self.time_writer('Zipping output resource %s ...' % folder)
+                # Zip the DICOMs output:
+                initdir = os.getcwd()
+                # Zip all the files in the directory
+                zip_name = os.path.join(fpath, '%s.zip' % folder)
+                os.chdir(fpath)
+                os.system('zip -r %s * > /dev/null' % zip_name)
+                # return to the initial directory:
+                os.chdir(initdir)
 
     def finish(self):
         """Method to copy the results in dax.RESULTS_DIR."""
-        results_dict = {'PDF': 'pdfpath'
-                        #
-                        # ADD OTHER RESULTS YOU WANT TO SAVE
-                        #
-                        }
+        output_folder = os.path.join(self.jobdir, 'outputs')
+        results_dict = {'PDF': self.pdf_final}
+        for folder in os.listdir(output_folder):
+            fpath = glob.glob(os.path.join(output_folder, folder, '*.zip'))
+            results_dict[folder] = fpath
+
         self.upload_dict(results_dict)
         self.end()
+
+    def run_matlab(self, matlab_script, verbose=False):
+        """Call MATLAB with -nodesktop -nosplash and -singlecompthread.
+
+        :param matlab_script: Full path to the .m file to run
+        :param verbose: True to print all MATLAB output to terminal, False to
+         suppress.
+        :return: None
+        """
+        self.time_writer("Matlab script: %s running ..." % matlab_script)
+        cmd = "matlab -nodisplay -nodesktop -nosplash -singleCompThread \
+    < %s" % matlab_script
+        if not verbose:
+            matlabdir = os.path.dirname(matlab_script)
+            prefix = os.path.basename(matlab_script).split('.')[0]
+            cmd = cmd+' > '+os.path.join(matlabdir, prefix+'_outlog.log')
+        os.system(cmd)
+        self.time_writer("Matlab script: %s done" % matlab_script)
+
+
+def is_dicom(fpath):
+    """check if the file is a DICOM medical data.
+
+    :param fpath: path of the file
+    :return boolean: true if it's a DICOM, false otherwise
+    """
+    file_call = '''file {fpath}'''.format(fpath=fpath)
+    output = sb.check_output(file_call.split())
+    if 'dicom' in output.lower():
+        return True
+
+    return False
+
+
+def get_dicom_list(directory):
+    """get the list of DICOMs file from the directory.
+
+    :param directory: directory containing the DICOM files.
+    :return list(): list of filepaths that are dicoms in directory
+    """
+    fnames = os.listdir(directory)
+    dicom_paths = list()
+    for fname in fnames:
+        fpath = os.path.join(directory, fname)
+        if is_dicom(fpath):
+            dicom_paths.append(fpath)
+
+    return dicom_paths
 
 
 if __name__ == '__main__':
     args = parse_args()
     # generate spider object:
     spider_obj = Spider_ADC_MAP(spider_path=sys.argv[0],
-                               jobdir=args.temp_dir,
-                               xnat_project=args.proj_label,
-                               xnat_subject=args.subj_label,
-                               xnat_session=args.sess_label,
-                               xnat_scan=args.scan_label,
-                               xnat_host=args.host,
-                               xnat_user=args.user,
-                               xnat_pass=None,
-                               suffix=args.suffix)
+                                jobdir=args.temp_dir,
+                                xnat_project=args.proj_label,
+                                xnat_subject=args.subj_label,
+                                xnat_session=args.sess_label,
+                                xnat_scan=args.scan_label,
+                                matlab_code=args.matlab_code,
+                                xnat_host=args.host,
+                                xnat_user=args.user,
+                                xnat_pass=None,
+                                suffix=args.suffix)
     # print some information before starting
-    spider_obj.print_init(args, "Benjamin Yvernault & Vasia Papoutsaki", "b.yvernault@ucl.ac.uk")
+    spider_obj.print_init(args, "Benjamin Yvernault & Vasia Papoutsaki",
+                          "b.yvernault@ucl.ac.uk")
 
     # Pre-run method to download data from XNAT
     spider_obj.pre_run(args)
