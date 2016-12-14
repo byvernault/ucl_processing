37eae750a550071561143a77ff15828131cfb479
byvernault <byvernault@gmail.com>
2016-12-14 17:54:59 +0000

    Editing matlab code path.

--- ucl_processors/Processor_ADC_MAP.py
+++ ucl_processors/Processor_ADC_MAP.py
@@ -28,7 +28,7 @@ DEFAULT_SPIDER_PATH = os.path.join(HOME, 'Xnat-management/ucl_processing/\
 ucl_spiders/', 'Spider_ADC_MAP_v1_0_0.py')
 DEFAULT_WALLTIME = '00:30:00'
 DEFAULT_MEM = 6048
-DEFAULT_MATLAB_CODE = os.path.join(HOME, 'Code', 'matlab')
+DEFAULT_MATLAB_CODE = os.path.join(HOME, 'Code', 'matlab', 'ADC_HAND', 'v1')
 DEFAULT_SCAN_TYPES = ['ep2d_DWI_ax_high_res$']
 
 # Format for the spider command line
